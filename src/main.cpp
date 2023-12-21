23a531deb1652d7e1be427ab2a3cf24ba74de073
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-12-21 16:47:43 +0800

    feat[src]: First single program

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// Include OpenCV, numCpp
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+
+int main(int argc, char** argv) {
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
+
